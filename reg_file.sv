73ee6b03577d46f296d26d3f3daf1a98262b377c
xkunD <103330637+xkunD@users.noreply.github.com>
2023-02-15 20:23:15 +0000

    update regfile

--- reg_file.sv
+++ reg_file.sv
@@ -1,9 +1,14 @@
 module reg_file(
-input logic [3:0] RA1, RA2, A3,
+input logic [3:0] RA1,
+input logic [3:0] RA2,
+input logic [3:0] WA,
 input logic [7:0] ALUResult,
-input logic clk, write_enable,
-output logic [7:0] RD1, RD2,
-output logic [7:0] cpu_out);
+input logic clk,
+input logic write_enable,
+output logic [7:0] RD1,
+output logic [7:0] RD2,
+output logic [7:0] cpu_out7_0
+);
 
 logic [7:0] rf [0:15];
 
@@ -11,12 +16,14 @@ assign rf[0] = 8'b0;
 
 assign RD1 = rf[RA1];
 assign RD2 = rf[RA2];
-assign cpu_out = rf[15];
+assign cpu_out7_0 = rf[15];
+
 
 always_ff @(posedge clk) begin
-if (write_enable && (A3 > 0)) begin
-rf[A3] <= ALUResult;
-end
+    const int index = WA;
+    if (write_enable && (index > 0)) begin
+        rf[index] <= ALUResult;
+    end
 end
 
 endmodule
--- reg_file_tb.sv
+++ reg_file_tb.sv
@@ -1,36 +1,43 @@
-`timescale 1ns/1ps
+`timescale 1ns/1ps 
 `include "reg_file.sv"
+
 module reg_file_tb;
 
-logic [7:0] t_data_out1, t_data_out2, t_data_in;
 logic [3:0] t_RA1, t_RA2, t_WA;
-logic t_clk, t_reset, t_write_enable;
+logic [7:0] t_ALUResult, t_RD1, t_RD2, t_cpu_out;
+logic t_CLK, t_write_enable;
 
-reg_file DUT (t_data_out1, t_data_out2, t_RA1, t_RA2, t_WA, t_data_in, t_clk, t_reset, t_write_enable);
+reg_file reg_file_inst (t_RA1, t_RA2, t_WA, t_ALUResult, t_CLK, t_write_enable, t_RD1, t_RD2, t_cpu_out);
 
 initial begin
-// Stimulus generator
-t_RA1 = 4;
-t_RA2 = 6;
-t_WA = 10;
-t_data_in = 32;
-t_write_enable = 1;
-t_clk = 0;
-#5 t_clk = 1;
-#5 t_clk = 0;
-t_write_enable = 0;
-#5 t_clk = 1;
-#5 t_clk = 0;
-t_RA1 = 3;
-t_RA2 = 5;
+    // Stimulus generator
+    t_RA1 = 1;
+    t_RA2 = 2;
+    t_ALUResult = 8'b00010101;
+    t_WA = 1;
+    t_write_enable = 1;
+    #10 t_CLK = 1;
+    #10 t_CLK = 0;
+    #10 t_RA1 = 3;
+    #10 t_RA2 = 4;
+    #10 t_ALUResult = 8'b10101010;
+    #10 t_WA = 2;
+    #10 t_write_enable = 0;
+    #10 t_CLK = 1;
+    #10 t_CLK = 0;
+    #10 t_RA1 = 15;
+    #10 t_RA2 = 0;
+    #10 t_ALUResult = 8'b01010101;
+    #10 t_WA = 15;
+    #10 t_write_enable = 1;
+    #10 t_CLK = 1;
+    #10 t_CLK = 0;
 end
 
 initial begin
-// Response monitor
-$monitor("t_RA1 = %d, t_RA2 = %d, t_data_out1 = %d, t_data_out2 = %d, t_clk = %d",
-t_RA1, t_RA2, t_data_out1, t_data_out2, t_clk);
+    // Response monitor
+    $monitor("t_CLK = %b t_write_enable = %b t_RA1 = %d t_RA2 = %d t_WA = %d t_ALUResult = %b t_RD1 = %b t_RD2 = %b t_cpu_out = %b",
+            t_CLK, t_write_enable, t_RA1, t_RA2, t_WA, t_ALUResult, t_RD1, t_RD2, t_cpu_out);
 end
 
-endmodule`
-
-
+endmodule
