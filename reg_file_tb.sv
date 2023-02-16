07226100abdd474691bac974f843c79ffa1bbef7
xkunD <103330637+xkunD@users.noreply.github.com>
2023-02-16 21:22:43 +0000

    update regfile

--- reg_file_tb.sv
+++ reg_file_tb.sv
@@ -3,8 +3,6 @@
 
 module reg_file_tb;
 
-
-
 logic [3:0] t_RA1, t_RA2, t_WA;
 logic [7:0] ALUResult;
 logic t_clk, t_write_enable;
@@ -23,18 +21,18 @@ initial begin
   #10
   t_RA1 = 4'b0100;
   t_RA2 = 4'b1000;
-  t_WA = 4'b1100;
+  t_WA = 4'b1000;
   ALUResult = 8'b00100100;
   t_clk = 1'b0;
   //t_reset = 1'b1;
-  t_write_enable = 1'b0;
+  t_write_enable = 1'b1;
   #10
   t_RA1 = 4'b1010;
   t_RA2 = 4'b0001;
   t_WA = 4'b1111;
   ALUResult = 8'b11110001;
   //t_reset = 1'b0;
-  t_clk = 1'b1;
+  t_clk = 1'b0;
   t_write_enable = 1'b1;
 end
 
