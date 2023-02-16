9ec54840b11e8249f25c570c758ceac7abecf914
xkunD <103330637+xkunD@users.noreply.github.com>
2023-02-16 20:52:32 +0000

    regfile

--- reg_file.sv
+++ reg_file.sv
@@ -1,34 +1,16 @@
-module reg_file(
-input logic [3:0] RA1,
-input logic [3:0] RA2,
-input logic [3:0] WA,
-input logic [7:0] ALUResult,
-input logic clk,
-input logic write_enable,
-output logic [7:0] RD1,
-output logic [7:0] RD2,
-output logic [7:0] cpu_out
-);
+module reg_file(input logic [3:0] RA1, RA2, WA, 
+                        input logic [7:0] ALUResult,
+                        input logic clk, write_enable,
+                        output logic [7:0] RD1, RD2, cpu_out); 
 logic [7:0] rf [0:15];
+logic [3:0] zero;
 
-assign RD1 = rf[RA1];
-assign RD2 = rf[RA2];
+assign RD1 = rf[RA1]; 
+assign RD2 = rf[RA2]; 
 assign cpu_out = rf[15];
-assign rf[0] = 8'b0;
 
 always_ff @(posedge clk) 
-if (write_enable) 
-rf[WA] <= ALUResult;
+if (write_enable && WA > 0) 
+    rf[WA] = ALUResult;
+
 endmodule
-
-
-
-// notes:
-/* The line logic [7:0] rf [0:15]; is a declaration of a two-dimensional array of logic values with 8 bits each.
-
-The first dimension, rf[0:15], is an array with 16 elements, indexed from 0 to 15.
-
-The second dimension, logic [7:0], defines each element of the array as a vector of 8 bits, with the most significant bit as 7 and the least significant bit as 0.
-
-So, the entire declaration creates an array of 16 8-bit values, named rf. This can be used to model a register file in digital logic circuits, where each element of the array represents a single register with 8 bits of data.
-*/
--- reg_file_tb.sv
+++ reg_file_tb.sv
@@ -1,43 +1,46 @@
-`timescale 1ns/1ps 
+`timescale 1ns/1ps
 `include "reg_file.sv"
 
 module reg_file_tb;
 
-logic [3:0] t_RA1, t_RA2, t_WA;
-logic [7:0] t_ALUResult, t_RD1, t_RD2, t_cpu_out;
-logic t_CLK, t_write_enable;
 
-reg_file reg_file_inst (t_RA1, t_RA2, t_WA, t_ALUResult, t_CLK, t_write_enable, t_RD1, t_RD2, t_cpu_out);
+
+logic [3:0] t_RA1, t_RA2, t_WA;
+logic [7:0] ALUResult;
+logic t_clk, t_write_enable;
+logic [7:0] t_RD1, t_RD2, t_cpu_out;
+
+reg_file R1 (t_RA1, t_RA2, t_WA, ALUResult, t_clk,  t_write_enable, t_RD1, t_RD2, t_cpu_out);
 
 initial begin
-    // Stimulus generator
-    t_RA1 = 4'b0001;
-    t_RA2 = 4'b0010;
-    t_ALUResult = 8'b00000011;
-    t_WA = 4'b1010;
-    t_write_enable = 1'b1;
-    #10 t_CLK = 1;
-    #10 t_CLK = 0;
-    #10 t_RA1 = 3;
-    #10 t_RA2 = 4;
-    #10 t_ALUResult = 8'b10101010;
-    #10 t_WA = 2;
-    #10 t_write_enable = 0;
-    #10 t_CLK = 1;
-    #10 t_CLK = 0;
-    #10 t_RA1 = 15;
-    #10 t_RA2 = 0;
-    #10 t_ALUResult = 8'b01010101;
-    #10 t_WA = 15;
-    #10 t_write_enable = 1;
-    #10 t_CLK = 1;
-    #10 t_CLK = 0;
+  t_RA1 = 4'b0001;
+  t_RA2 = 4'b0010;
+  t_WA = 4'b1010;
+  ALUResult = 8'b00000011;
+  t_clk = 1'b1;
+  //t_reset = 1'b0;
+  t_write_enable = 1'b1;
+  #10
+  t_RA1 = 4'b0100;
+  t_RA2 = 4'b1000;
+  t_WA = 4'b1100;
+  ALUResult = 8'b00100100;
+  t_clk = 1'b0;
+  //t_reset = 1'b1;
+  t_write_enable = 1'b0;
+  #10
+  t_RA1 = 4'b1010;
+  t_RA2 = 4'b0001;
+  t_WA = 4'b1111;
+  ALUResult = 8'b11110001;
+  //t_reset = 1'b0;
+  t_clk = 1'b1;
+  t_write_enable = 1'b1;
 end
 
 initial begin
-    // Response monitor
-    $monitor("t_CLK = %b t_write_enable = %b t_RA1 = %d t_RA2 = %d t_WA = %d t_ALUResult = %b t_RD1 = %b t_RD2 = %b t_cpu_out = %b",
-            t_CLK, t_write_enable, t_RA1, t_RA2, t_WA, t_ALUResult, t_RD1, t_RD2, t_cpu_out);
+  $monitor ("t_RA1 = %d t_RA2 = %d t_WA = %d ALUResult = %d t_clk = %d t_write_enable = %d t_RD1 = %d t_RD2 = %d",
+            t_RA1, t_RA2, t_WA, ALUResult, t_clk,  t_write_enable, t_RD1, t_RD2);
 end
 
 endmodule
