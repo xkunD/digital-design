70e6ee695190b2a8e5cdd03d3e4ac7c456f37b68
xkunD <103330637+xkunD@users.noreply.github.com>
2023-02-18 15:28:21 +0000

    update rf[0] = 0

--- reg_file.sv
+++ reg_file.sv
@@ -3,16 +3,15 @@ module reg_file(input logic [3:0] RA1, RA2, WA,
                         input logic clk, write_enable,
                         output logic [7:0] RD1, RD2, cpu_out); 
 logic [7:0] rf [0:15];
-logic [3:0] zero;
 
 assign RD1 = rf[RA1]; 
 assign RD2 = rf[RA2]; 
 assign cpu_out = rf[15];
 
-assign rf[0] == 8'h0;
+rf[4'b0] = 8'b0;
 
 always_ff @(posedge clk) 
 if (write_enable && WA > 0) 
-    rf[WA] = ALUResult;
+    rf[WA] <= ALUResult;
 
 endmodule 
