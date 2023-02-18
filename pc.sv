094a5cf881945a64bf1f1a36c1706ca6380ea1a4
xkunD <103330637+xkunD@users.noreply.github.com>
2023-02-18 20:16:43 +0000

    update pc.sv pc_tb.sv pc_tb.vvp

--- pc.sv
+++ pc.sv
@@ -2,8 +2,8 @@ module pc (input logic[7:0] immediate,
             input logic PCSrc, CLK, reset,
             output logic[7:0] PC);
 always_ff @ (posedge CLK) begin
-    if (reset) count <= 8'b0; 
-    else if (PCSrc) count <= immediate;
-    else count <= count + 1; 
+    if (reset) PC <= 8'b0; 
+    else if (PCSrc) PC <= immediate;
+    else PC <= PC + 1; 
 end
 endmodule
--- pc_tb.sv
+++ pc_tb.sv
@@ -1,2 +1,38 @@
-`timescale 1ps/1ps
+`timescale 1ns/1ps
 `include "pc.sv"
+
+module pc_tb;
+logic [7:0] t_PC, t_immediate;
+logic t_PCSrc, clk, t_reset;
+
+
+pc pc(t_immediate, t_PCSrc, clk, t_reset, t_PC);
+
+initial begin
+    clk = 0;
+    forever #10 clk = ~clk;
+end
+
+initial begin
+    $dumpfile("pc_tb.vcd"); 
+    $dumpvars(0, pc_tb);
+    // Stimulus generator
+    t_reset = 1; t_PCSrc = 0; t_immediate = 8'b01;
+    #10;
+    t_reset = 0; t_PCSrc = 1;
+    #20
+    t_immediate = 8'b111;
+    #20
+    t_PCSrc = 0;
+    #50
+    t_reset = 1;
+    #20;
+end
+
+initial begin // Response monitor
+    $monitor ("t = %3d clk = %d, t_immediate = %b t_PCSrc = %b t_reset = %d t_PC = %d", $time, clk, t_immediate, t_PCSrc, t_reset, t_PC);
+    #120;
+    $finish; 
+end
+
+endmodule
